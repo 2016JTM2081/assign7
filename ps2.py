17969925c5d02e0e3348c8ec89d9c6a06b60d8c3
2016JTM2081 <bansaltanya0508@gmail.com>
2016-09-12 16:56:10 +0530

    updated

--- ps2.py
+++ ps2.py
@@ -1,3 +1,19 @@
 ###### this is the second .py file ###########
 
-####### write your code here ##########
+#!/bin/python
+import random
+import math
+j = 0.0
+i = 0.0
+for i in list(range(10)):      #iterarte for 10 times
+	(X,Y)=(random.random()*2- 1, random.random()*2-1)    #x and y are generated
+	c = (X ** 2 + Y ** 2)
+	d = math.sqrt( c )
+	i += 1
+	if d <= 1:
+		j += 1
+	
+print i                  #no. of points in the square
+print j                  #no. of points in the circle
+percent = (j/i) * 100
+print percent
